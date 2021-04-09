e6763c1c5a249e8b421569012a720e945f370c3d
gutalavijay1111 <67408312+gutalavijay1111@users.noreply.github.com>
2021-04-10 00:24:49 +0530

    pyaadhaar.decode error fix

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 
 def secureQR(request,QR):
-    from pyaadhaar.decode import AadhaarSecureQr
+    from pyaadhaar.deocde import AadhaarSecureQr
     obj  = AadhaarSecureQr(QR)
     print(obj.decodeddata())
     return JsonResponse(obj.decodeddata())
